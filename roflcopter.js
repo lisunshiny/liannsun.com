e0dea11e96f2fe1d7a53006809052df7d58286c0
Liann Sun <lisunshiny@gmail.com>
2016-07-06 00:44:45 -0400

    change to yahoo api and add favicon and seo

--- roflcopter.js
+++ roflcopter.js
@@ -1,18 +1,6 @@
 $(function() {
-  // Docs @ https://www.metaweather.com/api/
-  var weatherHexs = {
-    c: "#F2F0A1", // clear
-    sn: "#D9D9D6", // snow
-    sl: "#B6CFD0", // sleet
-    h: "#FFB3AB", // hail
-    t: "#DFA0C9", // thunderstorm
-    hr: "#9595D2", // heavy rain
-    lr: "#B4B5DF", // light rain
-    s: "#5BC2E7", // showers
-    hc: "#BEC6C4", // heavy clouds
-    lc: "#bbbdbe" // light clouds
-  },
-  weatherCodeMap = [
+  // Docs @ https://developer.yahoo.com/weather/documentation.html#codes
+  var weatherCodeMap = [
     [_.range(0, 9), "t"],
     [_.range(9, 13), "lr"],
     [_.range(13, 17), "sn"],
@@ -24,23 +12,7 @@ $(function() {
     [_.range(41, 44), "sn"],
     [_.range(44, 45), "c"],
     [_.range(45, 48), "t"],
-  ],
-  textSelector = "a";
-
-  function getWeather() {
-    $.ajax({
-      method: "GET",
-      dataType: "json",
-      url: "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(2459115)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys",
-      success: function(data) {
-        var conditionCode = data.query.results.channel.item.condition.code;
-        var cssClass = getClassFromWeatherCode(conditionCode);
-        $(textSelector).addClass(cssClass);
-      }
-    })
-  };
-
-  getWeather()
+  ];
 
   function getClassFromWeatherCode(code) {
     var cssClass;
@@ -59,5 +31,20 @@ $(function() {
     else {
       return "unk";
     }
-  };
+  }
+
+  $.ajax({
+    method: "GET",
+    dataType: "json",
+    url: "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(2459115)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys",
+    success: function(data) {
+      var conditionCode = data.query.results.channel.item.condition.code,
+      conditionText = data.query.results.channel.item.condition.text.toLowerCase(),
+      smallCopy = "Fun fact: the color of the links changes depending on the current weather in NYC (" + conditionText + ").",
+      cssClass = getClassFromWeatherCode(conditionCode),
+      $info = $("<small>").text(smallCopy);
+      $("a").addClass(cssClass);
+      $("body").append($info);
+    }
+  });
 });
